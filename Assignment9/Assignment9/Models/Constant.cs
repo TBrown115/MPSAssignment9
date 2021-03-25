9cac82eb4201ca87e5bf51c48e55c5944a96eab4
TBrown115 <70948046+TBrown115@users.noreply.github.com>
2021-03-26 00:15:22 +0200

    second commit

--- Assignment9/Assignment9/Models/Constant.cs
+++ Assignment9/Assignment9/Models/Constant.cs
@@ -8,5 +8,6 @@ namespace Assignment9.Models
     {
         public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
         public static string OpenWeatherMapAPIKey = "6d1b7042e1f41822d4208841485e8ba9";
+        
     }
 }
